72a3b54dbdca89eaaec878d3c73a2f0903c40682
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2022-02-08 23:09:54 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deck Builder</title>
+</head>
+<body>
+
+    <form action="login.php" method="post">
+    Username: <input type="text" name="username" value="<?php echo $username;?>"><br>
+    Password: <input type="text" name="password" value="<?php echo $password;?>"><br>
+    <input type="submit">
+    </form>
+    
+</body>
+</html>
